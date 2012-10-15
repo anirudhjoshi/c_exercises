61b05256878fa61006c720f9053fad02b2fbec92
Anirudh Joshi <github@anirudhjoshi.com>
2012-10-15 14:27:44 +1100

    added rand strings

--- sorting/main.c
+++ sorting/main.c
@@ -6,10 +6,30 @@
 
 #define MAX 10
 
+char* randomString(char *s, int size){
+
+    int i;
+    int randomLength = rand() % (size) + 1;
+
+    static const char pool[] = "123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (i = 0; i < randomLength; i++){
+
+        s[i] = pool[ rand() % (sizeof pool- 1)];
+
+    }
+
+    s[i] = '\0';
+
+    return s;
+
+}
+
 int main(){
 
     int i;
     int *data = malloc(sizeof(int) * MAX);
+    char* s = malloc(sizeof(char) * MAX);
 
     srand(time(NULL));
 
@@ -18,6 +38,10 @@ int main(){
     for (i = 0; i < MAX; i++) {
 
         int num = rand() % (1000 - 1);
+
+        randomString(s, MAX);
+
+        printf("%s\n", s);
         insert(list, num);
 
     }
